448377170e1bea6c07a140bde49f15a962e7a8b4
filopuuuuGRRRR <filopuGRRRR@gmail.com>
2017-04-21 14:53:29 +0200

    changement pour le blog/index/post

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,14 +1,36 @@
 var keystone = require('keystone');
 
-exports = module.exports = function (req, res) {
+exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
 
+	locals.data = {
+		posts: [],
+		categories: [],
+	};
+
+	view.on('init', function(next) {
+
+		var q = keystone.list('Post').paginate({
+				page: req.query.page || 1,
+				filters: {
+					state: 'published',
+				},
+			})
+			.sort('-publishedDate')
+			.populate('author');
+
+		q.exec(function(err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+	});
 	// Render the view
 	view.render('index');
 };
