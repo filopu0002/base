a0ed141fd3d777ed3af542c4f3c4290c06f9c8a7
filopu0002 <sylvainyner@macbook-pro-de-s.home>
2017-09-19 02:07:06 +0200

    sqdqsdqsd

--- public/js/insta.js
+++ public/js/insta.js
@@ -368,3 +368,13 @@
   });
 
 }).call(this);
+
+
+window.onload = function() {
+  var userFeed = new Instafeed({
+  get: 'user',
+  userId: '37460794',
+  accessToken: '37460794.c9aadc8.a2b34f53c98c4d19a10ecd5f8182e25a'
+  });
+  userFeed.run();
+}
