8a5b077f1e09a16ab19bd496c951865d3057a4e4
Filopu <filopu@gmail.com>
2018-05-25 11:25:10 +1200

    step 8.4 cloudi

--- models/Post.js
+++ models/Post.js
@@ -17,9 +17,9 @@ Post.add({
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, default: Date.now, index: true, dependsOn: { state: 'published' } },
 	dateArticle: { type: String},
-	image: { type: Types.CloudinaryImage,  folder: 'path/to/image' ,select : true, selectPrefix: 'path/to/images' },
-	//image: { type: Types.CloudinaryImage, select : true},
-	content: {
+	//image: { type: Types.CloudinaryImage,  folder: 'path/to/image' ,select : true, selectPrefix: 'path/to/images' },
+	image: { type: Types.CloudinaryImage},
+	contenty: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 },
 	},
