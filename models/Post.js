219c8f4e752615c18e73c59d3d3565c791aea682
Filopu <filopu@gmail.com>
2018-05-25 10:31:30 +1200

    comment

--- models/Post.js
+++ models/Post.js
@@ -13,19 +13,19 @@ var Post = new keystone.List('Post', {
 
 Post.add({
 	title: { type: String, required: true },
-	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	/*state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, default: Date.now, index: true, dependsOn: { state: 'published' } },
 	dateArticle: { type: String},
 	//image: { type: Types.CloudinaryImage,  folder: 'path/to/image' ,select : true, selectPrefix: 'path/to/images' },
-	image: { type: Types.CloudinaryImage, select : true},
-	content: {
+	//image: { type: Types.CloudinaryImage, select : true},
+	/*content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 },
 	},
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
 	ville: {type: String},
-	pays: { type: Types.Relationship, ref: 'Pays', many: true },
+	pays: { type: Types.Relationship, ref: 'Pays', many: true },*/
 });
 
 Post.schema.virtual('content.full').get(function () {
